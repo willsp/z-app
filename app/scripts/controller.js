9867fe8667d06067da5ac91659052866e529fe61
Phillip Wills <philwills@gmail.com>
2014-06-17 01:08:13 -0700

    Using generated video instead of video on the page

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -5,17 +5,28 @@
 
     var container = document.getElementById('scroller');
     var template = document.getElementById('itemTemplate').textContent;
-    var videoContainer = document.getElementById('videoInsert');
-    var videoInsert = videoContainer.children[0];
-    var video = document.getElementById('video');
+
+    var videoInsert = document.createElement('li');
+    videoInsert.setAttribute('class', 'video');
+    var video = document.createElement('video');
+    var webm = document.createElement('source');
+    webm.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.webm');
+    webm.setAttribute('type', 'video/webm');
+    var mp4 = document.createElement('source');
+    mp4.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.mp4');
+    mp4.setAttribute('type', 'video/mp4');
+    var ogv = document.createElement('source');
+    ogv.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.ogv');
+    ogv.setAttribute('type', 'video/ogv');
+
+    video.appendChild(webm);
+    video.appendChild(mp4);
+    video.appendChild(ogv);
+    videoInsert.appendChild(video);
 
     // Set maximum height for the scroller
     var height = window.innerHeight - 60 + 'px';
     container.setAttribute('style', 'height: ' + height);
-
-    // Don't display the video in it's original spot, hiding it here
-    // so it still shows if js is disabled.
-    videoContainer.setAttribute('style', 'display: none;');
 
     var playPauseVideo = function() {
         var videoLi;
