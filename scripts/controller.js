3d6da4cafea214643a2e555b6990ca62a3da3f14
Phillip Wills <philwills@gmail.com>
2014-06-17 18:25:59 -0700

    Update with generated video code

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,17 +5,31 @@
 
     var container = document.getElementById('scroller');
     var template = document.getElementById('itemTemplate').textContent;
-    var videoContainer = document.getElementById('videoInsert');
-    var videoInsert = videoContainer.children[0];
-    var video = document.getElementById('video');
 
-    // Set maximum height for the scroller
-    var maxHeight = window.innerHeight - 60 + 'px';
-    container.setAttribute('style', 'max-height: ' + maxHeight);
+    // Generate the video element to shiv in.
+    var videoInsert = document.createElement('li');
+    var video = document.createElement('video');
+    var webm = document.createElement('source');
+    var mp4 = document.createElement('source');
+    var ogv = document.createElement('source');
 
-    // Don't display the video in it's original spot, hiding it here
-    // so it still shows if js is disabled.
-    videoContainer.setAttribute('style', 'display: none;');
+    videoInsert.setAttribute('class', 'video');
+
+    webm.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.webm');
+    webm.setAttribute('type', 'video/webm');
+    mp4.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.mp4');
+    mp4.setAttribute('type', 'video/mp4');
+    ogv.setAttribute('src', 'http://video-js.zencoder.com/oceans-clip.ogv');
+    ogv.setAttribute('type', 'video/ogv');
+
+    video.appendChild(webm);
+    video.appendChild(mp4);
+    video.appendChild(ogv);
+    videoInsert.appendChild(video);
+
+    // Set height for the scroller to the height of the window - header
+    var height = window.innerHeight - 60 + 'px';
+    container.setAttribute('style', 'height: ' + height);
 
     var playPauseVideo = function() {
         var videoLi;
